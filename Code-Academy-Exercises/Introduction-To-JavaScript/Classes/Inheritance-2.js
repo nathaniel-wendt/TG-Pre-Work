88c233d42f0a4736e9da245a56fbb315a1e06567
Nathaniel <nathaniel.wendt@gmail.com>
2019-01-18 19:13:08 -0700

    Finish Classes lessons

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Classes/Inheritance-2.js
@@ -0,0 +1,17 @@
+class HospitalEmployee {
+  constructor(name) {
+    this._name = name;
+    this._remainingVacationDays = 20;
+  }
+  get name() {
+    return this._name;
+  }
+  get remainingVacationDays() {
+    return this._remainingVacationDays;
+  }
+  takeVacationDays(daysOff) {
+    this._remainingVacationDays = this._remainingVacationDays - daysOff;
+  }
+}
+
+// I learned how to create a parent class
