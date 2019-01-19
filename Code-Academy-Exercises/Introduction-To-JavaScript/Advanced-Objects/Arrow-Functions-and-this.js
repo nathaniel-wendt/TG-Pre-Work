1b9b32042e4049145541d2551e220e993f707142
Nathaniel <nathaniel.wendt@gmail.com>
2019-01-18 18:27:16 -0700

    Finish Advanced-Objects lessons

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Advanced-Objects/Arrow-Functions-and-this.js
@@ -0,0 +1,10 @@
+const robot = {
+  energyLevel: 100,
+  checkEnergy() {
+    console.log(`Energy is currently at ${this.energyLevel}%.`)
+  }
+}
+
+robot.checkEnergy();
+
+// avoid using arrow functions when using this in a method
---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Advanced-Objects/Factory-Functions.js
@@ -0,0 +1,15 @@
+const robotFactory = (model, mobile) => {
+  return {
+    model: model,
+    mobile: mobile,
+    beep() {
+      console.log('Beep Boop');
+    }
+  }
+};
+
+const tinCan = robotFactory('P-500',true);
+
+console.log(tinCan.beep());
+
+// A factory function is a function that returns an object and can be reused to make multiple object instances.
